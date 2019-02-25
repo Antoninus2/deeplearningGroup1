d91b289cd063c59cf2e6eedabc6e84aa39ac5ef5
steven314 <roses7@my.erau.edu>
2019-02-25 16:04:56 -0500

    Added functionality to Jarvis and Essay

--- src/deeplearningGroup1/Jarvis.java
+++ src/deeplearningGroup1/Jarvis.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Jarvis {
@@ -13,19 +14,70 @@ public class Jarvis {
 	
 	
 	public static void main(String args[]) {
+		Essay essay = new Essay(1, 200, 4, "starter topic");
+		essay.writeEssay("Hello, worldd !");
 		Jarvis j = new Jarvis();
 		j.loadWordList();
-		j.printWords(5);
+		
+		System.out.println("\nMisspellings:");
+		System.out.println(j.checkSpelling(essay));
+		
+		System.out.println("\nWithin word Limit:");
+		System.out.println(j.checkWordLimit(essay));
+		
+		System.out.println("\nChecking Grammar:");
+		System.out.println(j.checkGrammar(essay));
 	}
 	
 	public Jarvis() {
 		
 	}
 	
-	public void checkSpelling(Essay essay) {
-		
+	// Returns number of grammatical mistakes
+	public int checkGrammar(Essay e) {
+		String essay = e.getEssay();
+		int numBadGrammar = 0;
+
+		// Check for spaces before punctuation
+		String punctuation = ",.;:?/!";
+		for (int i = 1; i < essay.length(); i++) {
+//			System.out.println(essay.charAt(i) + " | " + punctuation.indexOf(essay.charAt(i)) + " | " + essay.substring(i-1, i).equals(" "));
+			if (punctuation.indexOf(essay.charAt(i)) >= 0 && essay.substring(i-1, i).equals(" ")) {
+				numBadGrammar++;
+				System.out.println("{" + essay.substring(i-1, i+1) + "}");
+			}
+		}
+		return numBadGrammar;
 	}
 	
+	// Returns true if within word limit, false otherwise
+	public boolean checkWordLimit(Essay e) {
+		String essay = e.getEssay();
+		return essay.length() >= e.getLength()[0] && essay.length() < e.getLength()[1];
+	}
+	
+	// Returns number of misspelled words
+	public int checkSpelling(Essay e) {
+		String essay = e.getEssay();
+		System.out.println("Essay: " + essay);
+		String[] words = essay.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+		System.out.println("Words:");
+		
+		int numMisspelled = 0;
+		boolean isRealWord;
+		for (int i = 0; i < words.length; i++) {
+			isRealWord = Arrays.stream(wordList).anyMatch(words[i]::equals);
+			if (!isRealWord) {
+				numMisspelled++;
+			}
+			System.out.println(words[i] + "; " + isRealWord + "; " + numMisspelled);
+			
+		}
+		
+		return numMisspelled;
+	}
+	
+	// Loads dictionary of words from text file as String[]
 	public void loadWordList() {
 		try {
 			Scanner sc = new Scanner(new File(fileNameWordList));
@@ -42,6 +94,7 @@ public class Jarvis {
 		}
 	}
 	
+	// Purely for debugging purposes
 	public void printWords(int n) {
 		for (int i = 0; i < n; i++) {
 			System.out.println(wordList[i]);
